91d01200f00d3ee69d7a64f131f057de349586e4
Louise Findlay <louise@louisefindlay.com>
2021-07-28 15:37:49 +0100

    Adding login auth code

--- github.js
+++ github.js
@@ -3,9 +3,26 @@ const github = require("octonode");
 const client = github.client();
 
 // Test GitHub API
-client.get("/users/pksunkara", {}, function (err, status, body, headers) {
-    console.log(body); //json object
-});
+//client.get("/users/pksunkara", {}, function (err, status, body, headers) {
+//    console.log(body);
+//});
 
-// Test js file call
-console.log("This works");
+github.auth
+    .config({
+        username: process.env.GITHUB_USERNAME,
+        password: process.env.GITHUB_PASSWORD,
+        otp: process.env.GITHUB_OTP,
+    })
+    .login(scopes, function (err, id, token, headers) {
+        if (id === null) {
+            console.log(err);
+        } else {
+            console.log(id, token);
+        }
+    });
+
+// Test GitHub API Auth
+var scopes = {
+    scopes: ["user", "repo", "gist"],
+    note: "admin script",
+};
