70a460c12260712da5ee7808719a6875991bbccc
Louise Findlay <louise@louisefindlay.com>
2021-07-28 15:56:47 +0100

    GitHub auth working with personal token

--- github.js
+++ github.js
@@ -1,28 +1,40 @@
 // Intialise GitHub API
 const github = require("octonode");
-const client = github.client();
-
-// Test GitHub API
-//client.get("/users/pksunkara", {}, function (err, status, body, headers) {
-//    console.log(body);
-//});
-
-github.auth
-    .config({
-        username: process.env.GITHUB_USERNAME,
-        password: process.env.GITHUB_PASSWORD,
-        otp: process.env.GITHUB_OTP,
-    })
-    .login(scopes, function (err, id, token, headers) {
-        if (id === null) {
-            console.log(err);
-        } else {
-            console.log(id, token);
-        }
-    });
-
-// Test GitHub API Auth
-var scopes = {
-    scopes: ["user", "repo", "gist"],
+const client = github.client(process.env.GITHUB_TOKEN);
+const scopes = {
+    scopes: ["user", "repo"],
     note: "admin script",
 };
+
+// GitHub API Methods
+// TODO: Remove those we don't use
+const ghme = client.me();
+const ghuser = client.user("pksunkara");
+const ghrepo = client.repo("pksunkara/hub");
+const ghorg = client.org("flatiron");
+const ghissue = client.issue("pksunkara/hub", 37);
+const ghmilestone = client.milestone("pksunkara/hub", 37);
+const ghlabel = client.label("pksunkara/hub", "todo");
+const ghpr = client.pr("pksunkara/hub", 37);
+const ghrelease = client.release("pksunkara/hub", 37);
+const ghgist = client.gist();
+const ghteam = client.team(37);
+const ghproject = client.project(37);
+const ghnotification = client.notification(37);
+
+const ghsearch = client.search();
+
+// Test GitHub API auth
+async function getPullRequests() {
+    const repo = client.repo("MLH-Fellowship/pod-3.1.3-portfolio");
+
+    const result = await repo.prsAsync({ per_page: 100 });
+    console.log(result);
+    return result[0];
+}
+
+const retrieveResults = async () => {
+    await getPullRequests();
+};
+
+retrieveResults();
