c5037176a1e2de9ef5b16f823780a92318a87bc9
Damir Temir <dtemi2@uis.edu>
2021-07-27 20:42:31 -0500

    Added initial bot client settings
    Now when you run `node .` in the directory, the bot will come online on discord

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () =>{
+    console.log('Encourage Bot is online');
+});
+
+client.login('ODY5NzQ1NzAyOTU4NDA3NzIx.YQCrzg.SBy99VDx3s4oFrFaTT5m5HUzzqU');
