729b511db91f470112254b5693bc011de1259a84
Louise Findlay <louise@louisefindlay.com>
2021-07-29 15:50:14 +0100

    Passed issue number to comment function

--- commands/github.js
+++ commands/github.js
@@ -12,35 +12,29 @@ const ghme = client.me();
 const ghsearch = client.search();
 
 // TODO: Refactor var content to variables - array? class? or just use content instead of client
+const repo = "MLH-Fellowship/pod-3.1.3-team-4";
+let issue = null;
 const ghuser = client.user("Inoxia25");
-const ghrepo = client.repo("MLH-Fellowship/pod-3.1.3-team-4");
-const ghissue = client.issue("MLH-Fellowship/pod-3.1.3-team-4", 7);
-const ghpr = client.pr("pksunkara/hub", 37);
-// Get PRs
-async function getPullRequests() {
-  const result = await ghrepo.prsAsync({ per_page: 100 });
-  return result[0].body;
-}
+const ghrepo = client.repo(repo);
 
 // Post Issue Comment
-async function postIssueComment() {
-  const result = await ghissue.createCommentAsync({
-    body: "A test command posted through the Gitcord Bot!",
+async function postComment(issue) {
+  const result = await client.issue(repo, issue).createCommentAsync({
+    body: "A test comment posted through the Gitcord Bot!",
   });
   // console.log("Your comment: " + result[0].body + " has been posted.");
   return result[0].body;
 }
 
-const retrieveResults = async () => {
-  await getPullRequests();
-  await postIssueComment();
+const retrieveResults = async (issue) => {
+  await postComment(issue);
 };
 
 //Function to post the comment and send a message in the server
 const func = {
   botMessage: function (message) {
     if (message === "github") {
-      retrieveResults();
+      retrieveResults(7);
       return this.channel.send("Your comment has been posted!");
     }
   },
