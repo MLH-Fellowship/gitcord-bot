285e3c2b73bcd0c78fea34b524a7a825ca409db3
Louise Findlay <louise@louisefindlay.com>
2021-07-30 09:58:50 +0100

    Added more comments for user issue number input

--- commands/github.js
+++ commands/github.js
@@ -22,14 +22,15 @@ const ghrepo = client.repo("MLH-Fellowship/pod-3.1.3-team-4");
 //Function to post the comment and send a message in the server
 const func = {
   botMessage: async function (message, args) {
-    console.log("The command posted is" + message);
+    console.log("The command posted is " + message);
 
     // -github: Enter personal token
     if (message === "github") {
       return this.channel.send("Enter your personal Github token with -github-info");
-    } else if (message=== 'github-info') {
+    // -github-info: Get contents of personal token
+    } else if (message === 'github-info') {
       if (!args.length) {
-        return this.channel.send(`You didn't provide any arguments, ${this.author}!`);
+        return this.channel.send(`You didn't provide a GitHub Personal Token, ${this.author}!`);
       }
       githubToken = args[0];
       client = github.client(githubToken);
@@ -37,13 +38,23 @@ const func = {
     // -github-comment-issue: Post comment
    } else if (message === "github-comment-issue") {
       // TODO: Get the number from user input
-      issue = 4;
+      return this.channel.send("Enter the number of the issue you'd like to comment on with -github-issue-number");
+    }
+
+    // -github-issue-number: Get issue or PR number from user to post comment on
+    if (!args.length) {
+      return this.channel.send(`You didn't provide any arguments, ${this.author}!`);
+    }
+     if (!args.length) {
+        return this.channel.send(`You didn't provide any arguments, ${this.author}!`);
+      } else {
+        issue = args[0];
       const result = await client.issue(repo, issue).createCommentAsync({
         body: "A test comment posted through the Gitcord Bot!",
       }).then(result => {
         return this.channel.send("Your comment: " + result[0].body + " has been posted.").catch(error => console.log(error));
       });
-    }
+      }
   },
 };
 module.exports = func;
