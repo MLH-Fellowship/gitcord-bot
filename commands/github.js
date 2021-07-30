0535db18b01bd0a49502248e686a5cd234b61043
Louise Findlay <louise@louisefindlay.com>
2021-07-30 10:47:35 +0100

    Refactored how command files link to main.js

--- commands/github.js
+++ commands/github.js
@@ -1,32 +1,34 @@
+module.exports = {
+	name: "github",
+	description: 'GitHub bot commands',
+	execute(message, args) {
+		// Intialise GitHub API
+    const github = require("octonode");
+    let githubToken= null;
+    let client = github.client(githubToken);
+    const scopes = {
+      scopes: ["user", "repo"],
+      note: "admin script",
+    };
 
-// Intialise GitHub API
-const github = require("octonode");
-let githubToken= null;
-let client = github.client(githubToken);
-const scopes = {
-  scopes: ["user", "repo"],
-  note: "admin script",
-};
+    // GitHub API Methods
+    const ghme = client.me();
+    const ghsearch = client.search();
 
-// GitHub API Methods
-const ghme = client.me();
-const ghsearch = client.search();
-
-// TODO: Refactor var content to variables - array? class? or just use content instead of client
-const repo = "MLH-Fellowship/pod-3.1.3-team-4";
-let issue = null;
-const ghuser = client.user("Inoxia25");
-const ghrepo = client.repo("MLH-Fellowship/pod-3.1.3-team-4");
+    // TODO: Refactor var content to variables - array? class? or just use content instead of client
+    const repo = "MLH-Fellowship/pod-3.1.3-team-4";
+    let issue = null;
+    const ghuser = client.user("Inoxia25");
+    const ghrepo = client.repo("MLH-Fellowship/pod-3.1.3-team-4");
 
 
-//Function to post the comment and send a message in the server
-const func = {
-  botMessage: async function (message, args) {
-    console.log("The command posted is " + message);
+    // Post the comment and send a message in the server
+    console.log("The user sent " + message.content);
 
     // -github: Enter personal token
-    if (message === "github") {
-      return this.channel.send("Enter your personal Github token with -github-info");
+    if (message.content === "-github") {
+      console.log("Github was posted");
+      return message.channel.send("Enter your personal Github token with -github-info");
 
     // -github-info: Get contents of personal token
     } else if (message === 'github-info') {
@@ -50,14 +52,6 @@ const func = {
         return this.channel.send("To post a comment on issue " + issue + " , use -github-post-comment followed by your message.").catch(error => console.log(error));
       }
     // -github-post-comment: Posts desired comment on previously specified issue/PR
-    } else if (message === "github-post-comment") {
-      // TODO: Add some fallback if token hasn't been set
-      const result = await client.issue(repo, issue).createCommentAsync({
-        body: args[0],
-      }).then(result => {
-        return this.channel.send("Your comment: " + result[0].body + " has been posted.").catch(error => console.log(error));
-      });
     }
   },
 };
-module.exports = func;
--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
+
+const fs = require("fs");
 const Discord = require("discord.js");
+
 const client = new Discord.Client();
-const tester = require("./commands/testing");
-const githubCommands = require("./commands/github");
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 const prefix = "-";
 
 // checking if bot is ready
@@ -17,11 +26,14 @@ client.on("message", (message) => {
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command.includes("github")) { 
-        githubCommands.botMessage.call(message, command, args);
-    } else {
-        tester.botMessage.call(message, command, args);
-    }
+    if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 // bot token
