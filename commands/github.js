9b9dbcfd9cbf7bc3656c6e3afaafdea28a9860b7
Louise Findlay <louise@louisefindlay.com>
2021-07-30 12:18:24 +0100

    WIP Working fine except for github-info reply

--- commands/github.js
+++ commands/github.js
@@ -29,15 +29,14 @@ module.exports = {
       return message.reply("use -github-info with your personal Github token to continue.");
 
     // -github-info: Get contents of personal token
-    }
-
-    if (command === "github-info") {
+    } else if (command === "github-info") {
       if (!args.length) {
         return message.reply("you didn't provide a GitHub Personal Token.");
-      }
+      } else {
       githubToken = args[0];
       client = github.client(githubToken);
       return message.reply("GitHub auth was successful. Use -github-issue-number with the number of the issue you'd like to comment on.");
+      }
     }
 
     // -github-issue-number: Get issue/PR number from user
@@ -51,12 +50,12 @@ module.exports = {
       }
     // -github-post-comment: Posts desired comment on previously specified issue/PR
     } else if (command === "github-post-comment") {
-      postComment();
+      postComment(issue);
     }
   }
 };
 
-async function postComment() { 
+async function postComment(issue) { 
     await client.issue(repo, issue).createCommentAsync({
     body: "A test comment posted through the Gitcord Bot!",
     }).then(result => {
