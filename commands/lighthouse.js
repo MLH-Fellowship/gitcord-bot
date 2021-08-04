dbbe90e02750eeda4907e35d706521506eb592d7
Inoxia25 <nandinisapna1401@gmail.com>
2021-08-04 19:59:50 +0530

    connected lighthouse with bot command

--- commands/lighthouse.js
+++ commands/lighthouse.js
@@ -1,26 +1,31 @@
 const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-
-module.exports = {
-	name: "lighthouse",
-	description: 'Command used to get lighthouse stats for a site',
-	execute(command, message) {
-(async () => {
+async function getStats(url){
   const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
   const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
-  const runnerResult = await lighthouse('https://example.com', options);
-
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-  fs.writeFileSync('lhreport.html', reportHtml);
+  const runnerResult = await lighthouse(url, options);
+  // // `.report` is the HTML report as a string
+  // const reportHtml = runnerResult.report;
+  // fs.writeFileSync('lhreport.html', reportHtml);
 
   // `.lhr` is the Lighthouse Result as a JS object
   console.log('Report is done for', runnerResult.lhr.finalUrl);
   console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
   await chrome.kill();
-})();
+}
+module.exports = {
+	name: "lighthouse",
+	description: 'Command used to get lighthouse stats for a site',
+	execute(command, message,args) {
+    if (!args.length) {
+      return message.reply("Please provide the site url to get its lighthouse stats.");
+  } else {
+      let url = args[0];
+      getStats(url);
+      return message.reply("Got the lighthouse stats.");
   }
+}
 }
 
