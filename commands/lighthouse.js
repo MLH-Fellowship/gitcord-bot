56298e0b52f9bdbeed0d93341116792c1434c95f
Inoxia25 <nandinisapna1401@gmail.com>
2021-08-04 19:53:36 +0530

    Added lighthouse.js

---
+++ commands/lighthouse.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+module.exports = {
+	name: "lighthouse",
+	description: 'Command used to get lighthouse stats for a site',
+	execute(command, message) {
+(async () => {
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+  const runnerResult = await lighthouse('https://example.com', options);
+
+  // `.report` is the HTML report as a string
+  const reportHtml = runnerResult.report;
+  fs.writeFileSync('lhreport.html', reportHtml);
+
+  // `.lhr` is the Lighthouse Result as a JS object
+  console.log('Report is done for', runnerResult.lhr.finalUrl);
+  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+  await chrome.kill();
+})();
+  }
+}
+
