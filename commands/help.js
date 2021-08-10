42cbfbacf76b62e367f5cd8f88bc98a6f8dbdd16
Inoxia25 <nandinisapna1401@gmail.com>
2021-08-10 19:08:29 +0530

    added usage to help command

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,14 @@
 module.exports = {
     name: "help",
-    description: "Help bot commands",
+    description: "Gives description and usage for each of the commands.",
+    usage: "-help will give you a list of all commands, and -help <command-name> will give you details about a command.",
     execute(command, message, args) {
         const data = [];
         const { commands } = message.client;
         if (!args.length) {
             data.push("Here's a list of all my commands:");
             data.push(commands.map((command) => command.name).join("\n"));
-            data.push("\nYou can send -help [command name] to get info on a specific command!");
+            data.push("\nYou can send -help [command name] to get info on a specific command.");
 
             return message
                 .reply(data, { split: true })
@@ -20,6 +21,7 @@ module.exports = {
 
         const name = args[0].toLowerCase();
         let listCommand = null;
+        const prefix = "-";
         commands.map((cmnd) => {
             if (cmnd.name === name) listCommand = cmnd;
         });
@@ -27,10 +29,10 @@ module.exports = {
         if (!listCommand) {
             return message.reply("that's not a valid command!");
         }
-        data.push("**Name: **" + listCommand.name);
+        data.push(`Details about the ${listCommand.name} command are :\n**Name: **${listCommand.name}`);
 
-        if (listCommand.description) data.push("**Description: **" + listCommand.description);
-
+        if (listCommand.description) data.push(`**Description: **${listCommand.description}`);
+        if (listCommand.usage) data.push(`**Usage: **${listCommand.usage}`);
         message.reply(data, { split: true });
     },
 };
