aa67623ad3f48cc0291525f88daa7be77bf47189
louisefindlay23 <louise@louisefindlay.com>
2021-08-10 15:59:21 +0000

    Database result is now returned through async function as var

--- commands/github-info.js
+++ commands/github-info.js
@@ -16,27 +16,15 @@ module.exports = {
 
         // -github-info: Get contents of personal token
         if (command === "github-info") {
-            if (!args.length) {
-                // const token = await fetchGitHubToken(message.author.id);
-
-                const getToken = fetchGitHubToken(message.author.id).then(token => {
-                    if (token.length !== 0){
-                        return message.reply("Your GitHub token is on file: " + token[0].dataValues.token);
-                    } else {
-                        return message.reply("Your GitHub token isn't on file. Try again, specifying your GitHub token (once you do it, we'll store it securely)")
-                    }
-                });
-                console.log("YOUR TOKEN IS", getToken);
-            } else {
-                githubToken = args[0];
-                insertGitHubToken(message.author.id, githubToken).then(result => {
-                    console.log(result);
-                })
-                writeToken(githubToken);
-                return message.reply(
-                    "GitHub auth was successful. Use -github-issue-number with the number of the issue you'd like to comment on."
-                );
-            }
+            const fetchGit = async () => {
+                let newToken = await fetchGitHubToken(message.author.id);
+                console.log(newToken);
+            };
+            fetchGit();
+            // const fetchGit = fetchGitHubToken(message.author.id).then(//(result) => {
+            //      return result;
+            //  });
+            // console.log(fetchGit);
         }
     },
 };
@@ -50,52 +38,49 @@ require("dotenv").config();
 
 // Connect to CockroachDB through Sequelize.
 var sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "damir",
-  password: process.env.COCKROACH_DB_PASSWORD,
-  host: process.env.COCKROACH_DB_HOST,
-  port: 26257,
-  database: process.env.COCKROACH_DB_DATABASE,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-      // For secure connection:
-      ca: fs.readFileSync('certs/cc-ca.crt')
-                 .toString()
+    dialect: "postgres",
+    username: "damir",
+    password: process.env.COCKROACH_DB_PASSWORD,
+    host: process.env.COCKROACH_DB_HOST,
+    port: 26257,
+    database: process.env.COCKROACH_DB_DATABASE,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+            // For secure connection:
+            ca: fs.readFileSync("certs/cc-ca.crt").toString(),
+        },
     },
-  },
-  logging: false,
+    logging: false,
 });
 
 // Define the Tokens model for the "defaultdb" table.
 const Tokens = sequelize.define("defaultdb", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-  },
-  token: {
-    type: Sequelize.STRING,
-  },
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+    },
+    token: {
+        type: Sequelize.STRING,
+    },
 });
 
 // Get a token with user Discord ID
 async function fetchGitHubToken(discord_id) {
-  return await Tokens.findAll({
-    where: {
-      id: discord_id,
-    }
-  }).then((result) => {
-  console.log(result);
-  return result;
-  });
+    return await Tokens.findAll({
+        where: {
+            id: discord_id,
+        },
+    }).then((result) => {
+        return result;
+    });
 }
 
 // Insert new a token with user Discord ID and passed argument
-async function insertGitHubToken(discord_id, github_token){
+async function insertGitHubToken(discord_id, github_token) {
     return await Tokens.create({
         id: discord_id,
         token: github_token,
-    })
+    });
 }
-
