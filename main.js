b31d4d6c6f63d8dc8fdb804c06857bd192736b5f
Louise Findlay <louise@louisefindlay.com>
2021-08-12 16:30:01 +0100

    Add if statement so GitHub Token doesn't run for non-GitHub commands

--- main.js
+++ main.js
@@ -14,42 +14,49 @@ const { restEndpointMethods } = require("@octokit/plugin-rest-endpoint-methods")
 const MyOctokit = Octokit.plugin(restEndpointMethods);
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 client.once("ready", () => {
-    console.log("GitCord Bot is online");
+  console.log("GitCord Bot is online");
 });
 
 // taking in the commands
 client.on("message", (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(" ");
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-    // Message and Command logging
-    console.log("Message is:", message.content);
-    console.log("Command is:", command);
+  // Message and Command logging
+  console.log("Message is:", message.content);
+  console.log("Command is:", command);
 
-    if (!client.commands.has(command)) {
-        console.log("Command does not exist.");
-        return;
-    }
+  if (!client.commands.has(command)) {
+    console.log("Command does not exist.");
+    return;
+  }
 
+  if (command !== "lighthouse" || command !== "tech-stack" || command !== "help") {
     try {
-        db.fetchGit(message.author.id).then((result) => { // fetch GitHub token and initialize Octokit
-            let octokit = new MyOctokit({ auth: result });
-            client.commands.get(command).execute(command, message, args, octokit);
-        }).catch((err) => {
-            console.error(err);
-            return message.reply("Your GitHub token isn't on file. Run -github-info, specifying your GitHub token (and then try this again)");
+      db.fetchGit(message.author.id)
+        .then((result) => {
+          // fetch GitHub token and initialize Octokit
+          let octokit = new MyOctokit({ auth: result });
+          client.commands.get(command).execute(command, message, args, octokit);
+        })
+        .catch((err) => {
+          console.error(err);
+          return message.reply(
+            "Your GitHub token isn't on file. Run -github-info, specifying your GitHub token (and then try this again)"
+          );
         });
     } catch (error) {
-        console.error(error);
-        message.reply("There was an error trying to execute that command. Please try again.");
+      console.error(error);
+      message.reply("There was an error trying to execute that command. Please try again.");
     }
+  }
 });
 
 // bot token
