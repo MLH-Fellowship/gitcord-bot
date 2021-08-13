a15cf3b02f84dad1313fb2768f0e910f3246f9f1
Damir Temir <dtemi2@uis.edu>
2021-08-12 20:48:46 -0500

    Fixed a typo in main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ client.on("message", (message) => {
     return;
   }
 
-  if (command !== "lighthouse" && command !== "tech-stack" && command !== "help" && command !== "github-info" && command !== 'github-delete-command') {
+  if (command !== "lighthouse" && command !== "tech-stack" && command !== "help" && command !== "github-info" && command !== 'github-delete-token') {
     try {
       db.fetchGit(message.author.id)
         .then((result) => {
